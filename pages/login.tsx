69ea180c6d7db3f9aad51bf0563f6ad8bde1bca3
damoski2 <akogunoyindamola42@gmail.com>
2023-11-06 19:11:04 +0100

    chore: demo login account

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,7 +6,7 @@ import logo from "../assets/images/logo.png";
 
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=0c064b242e744e0ca6d6dbbc5458c704&response_type=code&redirect_uri=${
   process.env.NODE_ENV === "development"
-    ? 'http://localhost:3000'
+    ? "http://localhost:3000"
     : "https://apple-music-cloned.vercel.app/"
 }&scope=streaming%20user-read-email%20user-read-recently-played%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public%20playlist-modify-private`;
 
@@ -17,7 +17,20 @@ const login = () => {
 
       <div className="line w-full h-[1px] bg-[#004350] mt-8" />
 
-      <div className="h-[80vh] w-full flex justify-center items-center">
+      <div className="mt-8">
+        <p className="text-[#004350] text-center text-2xl font-semibold">
+          Can only work with this account below
+        </p>
+        <p className="text-center text-lg font-semibold mt-2">
+          <span className="text-[#FF293F]">email</span>: oyindacodes2@gmail.com
+        </p>
+        <p className="mt-2 text-center text-lg font-semibold">
+          {" "}
+          <span className="text-[#FF293F]">password</span>: password123
+        </p>
+      </div>
+
+      <div className="h-[60vh] w-full flex justify-center items-center">
         <Link
           className="bg-[#FF293F] text-white px-8 py-4 rounded-lg decoration-[#fff] hover:decoration-[#fff]"
           href={AUTH_URL}
